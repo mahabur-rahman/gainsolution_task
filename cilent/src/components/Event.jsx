bb67233e0dd2db60e32764f298b363c493a132b2
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-11-23 13:32:52 +0600

    feat: dynamic routing when user click on individual event

--- cilent/src/components/Event.jsx
+++ cilent/src/components/Event.jsx
@@ -1,29 +1,31 @@
 /* eslint-disable react/prop-types */
 import { Card, Col, Badge } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Event = ({ event }) => {
   return (
     <>
       <Col xl={4} lg={6} md={4} className="my-2" key={event._id}>
-        <Card style={{minHeight: "220px"}}>
-            {event.photo && (
-          <img className="postImg" src={event.photo} alt={event.title} />
-
-            )}
-          <Card.Body>
-            <Card.Title>{event.title}</Card.Title>
-            <Card.Text>{event.description}</Card.Text>
-            <Badge pill bg="light" text="dark">
-              <div className="text-secondary text-lg">
-                Start Date: {event.startDate}
-              </div>
-            </Badge>
-            <Badge pill bg="light" text="dark">
-              <div className="text-secondary text-sm">
-                End Date: {event.endDate}
-              </div>
-            </Badge>
-          </Card.Body>
+        <Card style={{ minHeight: "220px" }}>
+          {event.photo && (
+            <img className="postImg" src={event.photo} alt={event.title} />
+          )}
+          <Link to={`/event/${event._id}`}>
+            <Card.Body>
+              <Card.Title>{event.title}</Card.Title>
+              <Card.Text>{event.description}</Card.Text>
+              <Badge pill bg="light" text="dark">
+                <div className="text-secondary text-lg">
+                  Start Date: {event.startDate}
+                </div>
+              </Badge>
+              <Badge pill bg="light" text="dark">
+                <div className="text-secondary text-sm">
+                  End Date: {event.endDate}
+                </div>
+              </Badge>
+            </Card.Body>
+          </Link>
         </Card>
       </Col>
     </>
