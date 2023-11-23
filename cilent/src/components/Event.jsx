de51cd64d401677367fefe99ed9e6e173a895f46
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-11-23 13:25:58 +0600

    feat: event image show for dynamic

--- cilent/src/components/Event.jsx
+++ cilent/src/components/Event.jsx
@@ -1,17 +1,28 @@
 /* eslint-disable react/prop-types */
-import { Card, Col } from "react-bootstrap";
+import { Card, Col, Badge } from "react-bootstrap";
 
-const Event = ({event}) => {
+const Event = ({ event }) => {
   return (
     <>
       <Col xl={4} lg={6} md={4} className="my-2" key={event._id}>
-        <Card>
-          <Card.Img variant="top" src="holder.js/100px180" />
+        <Card style={{minHeight: "220px"}}>
+            {event.photo && (
+          <img className="postImg" src={event.photo} alt={event.title} />
+
+            )}
           <Card.Body>
             <Card.Title>{event.title}</Card.Title>
             <Card.Text>{event.description}</Card.Text>
-            <div className="text-secondary text-lg">Start Date: {event.startDate}</div>
-            <div className="text-secondary text-sm">End Date: {event.endDate}</div>
+            <Badge pill bg="light" text="dark">
+              <div className="text-secondary text-lg">
+                Start Date: {event.startDate}
+              </div>
+            </Badge>
+            <Badge pill bg="light" text="dark">
+              <div className="text-secondary text-sm">
+                End Date: {event.endDate}
+              </div>
+            </Badge>
           </Card.Body>
         </Card>
       </Col>
