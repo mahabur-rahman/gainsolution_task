82893f656dfb252ffc9e371768a2606f678b7208
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-11-23 12:39:01 +0600

    feat: category logic added in controller

--- api/index.js
+++ api/index.js
@@ -1,31 +1,32 @@
-const express = require('express')
-const app = express()
-const colors = require('colors')
+const express = require("express");
+const app = express();
+const colors = require("colors");
 const cors = require("cors");
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 
-// env config 
-dotenv.config()
-const PORT = process.env.PORT || 5000
+// env config
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 // connect with mongodb atlas
 const connectedDB = require("./db/connect");
 connectedDB();
 
-// ROUTE  
-const authRoute = require('./routes/auth.route')
-const userRoute = require('./routes/users.route')
-const eventRoute = require('./routes/events.route')
+// ROUTE
+const authRoute = require("./routes/auth.route");
+const userRoute = require("./routes/users.route");
+const eventRoute = require("./routes/events.route");
+const categoryRoute = require("./routes/category.route");
 
-// middleware 
-app.use(cors())
-app.use(express.json())
+// middleware
+app.use(cors());
+app.use(express.json());
 
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/events", eventRoute);
+app.use("/api/categories", categoryRoute);
 
-app.use('/api/auth', authRoute)
-app.use('/api/users',userRoute)
-app.use('/api/events', eventRoute)
-
-// listen 
-app.listen(PORT, () =>{
-    console.log(`Server is running at http://localhost:${PORT}`)
-})
+// listen
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
